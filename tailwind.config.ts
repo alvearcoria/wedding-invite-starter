9dc3fb4a9777c4ac41ce3cb90fcd6d4560eaa78a
Alexander Alvear Coria <alexalvear13@gmail.com>
2025-09-03 18:26:14 +0000

    Mejora la tipografia del resto del contenido

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['Playfair Display', 'serif'],
+        body: ['Montserrat', 'sans-serif'],
+        headline: ['Cormorant Garamond', 'serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
