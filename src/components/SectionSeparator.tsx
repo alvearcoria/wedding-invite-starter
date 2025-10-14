fd5dd9ce59cc4e742ca65fd8adfcd18534f8027f
alvearcoria <alexalvear13@gmail.com>
2025-10-14 23:16:17 +0000

    Sigue muy separada, pero ahorita note que el diseño que te solicite ante

--- src/components/SectionSeparator.tsx
+++ src/components/SectionSeparator.tsx
@@ -15,13 +15,13 @@ export function SectionSeparator({
     flip = false,
 }: SectionSeparatorProps) {
   return (
-    <div className={cn("relative -mb-px w-full h-16 md:h-20", bgColor, className)}>
+    <div className={cn("relative -mb-px w-full h-8 md:h-10 z-10", bgColor, className)}>
       <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none">
         <svg
           viewBox="0 0 1440 100"
           preserveAspectRatio="none"
           xmlns="http://www.w3.org/2000/svg"
-          className={cn("relative block h-[50px] w-[calc(100%+1.3px)] md:h-[75px]", waveColor, flip && "transform -scale-x-100")}
+          className={cn("relative block h-[25px] w-[calc(100%+1.3px)] md:h-[40px]", waveColor, flip && "transform -scale-x-100")}
         >
           <path d="M1200,50C1000,150,800,0,600,50C400,100,200,-50,0,50V100H1440V50H1200Z" />
         </svg>
