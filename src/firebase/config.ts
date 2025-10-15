4c1b8e3323b48fd2d12aae137d05f18f2f5ec7ec
alvearcoria <alexalvear13@gmail.com>
2025-10-15 00:13:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "mely-noe-wedding-celebration",
+  "appId": "1:643335247531:web:f85f0d69da05d7d46389ee",
+  "apiKey": "AIzaSyBtHhx8hKNh-6cA_XI4jnAY6ihOtKx-Qz0",
+  "authDomain": "mely-noe-wedding-celebration.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "643335247531"
+};
