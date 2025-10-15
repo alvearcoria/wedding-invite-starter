868a5324289407ab0218b9dcdfffaee9a7187556
alvearcoria <alexalvear13@gmail.com>
2025-10-15 19:42:13 +0000

    quiero que se vea una transicion asi pero alreves y que no sea negro en

--- src/components/SectionSeparator.tsx
+++ src/components/SectionSeparator.tsx
@@ -3,27 +3,23 @@ import { cn } from "@/lib/utils";
 
 interface SectionSeparatorProps {
   waveColor?: string;
-  bgColor?: string;
   className?: string;
 }
 
 export function SectionSeparator({ 
     waveColor = 'fill-background', 
-    bgColor = 'bg-transparent',
     className,
 }: SectionSeparatorProps) {
   return (
-    <div className={cn("w-full h-8 md:h-10 -mb-[1px]", bgColor, className)}>
-      <div className="h-full w-full overflow-hidden leading-none">
+    <div className={cn("w-full h-12 md:h-16 -mb-px", className)}>
         <svg
           viewBox="0 0 1440 100"
           preserveAspectRatio="none"
           xmlns="http://www.w3.org/2000/svg"
-          className={cn("block h-full w-[calc(100%+1.3px)]", waveColor)}
+          className={cn("block h-full w-full", waveColor)}
         >
-          <path d="M1200,50C1000,150,800,0,600,50C400,100,200,-50,0,50V100H1440V50H1200Z" />
+          <path d="M1440,0V100H0V0C240,60,480,100,720,100C960,100,1200,60,1440,0Z" />
         </svg>
-      </div>
     </div>
   );
 }
