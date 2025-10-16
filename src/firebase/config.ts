caa915b3a3b9301f0b25ff226068a1e4f528a5f0
alvearcoria <alexalvear13@gmail.com>
2025-10-16 16:01:10 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "mely-noe-wedding-celebration",
   "appId": "1:643335247531:web:f85f0d69da05d7d46389ee",
+  "storageBucket": "mely-noe-wedding-celebration.appspot.com",
   "apiKey": "AIzaSyBtHhx8hKNh-6cA_XI4jnAY6ihOtKx-Qz0",
   "authDomain": "mely-noe-wedding-celebration.firebaseapp.com",
   "measurementId": "",
