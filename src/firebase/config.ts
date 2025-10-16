2ac5b1d61d4423723dd83c6c756e5d46aef617fb
alvearcoria <alexalvear13@gmail.com>
2025-10-16 17:33:38 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  "projectId": "wedding-site-73595-2",
-  "appId": "1:936359053330:web:10c85b13689452b4742a73",
-  "storageBucket": "wedding-site-73595-2.appspot.com",
-  "apiKey": "AIzaSyCV3-73TqrKqH1t_O2a37r5dF89u-sLd-w",
-  "authDomain": "wedding-site-73595-2.firebaseapp.com",
-  "messagingSenderId": "936359053330"
+  "projectId": "mely-noe-wedding-celebration",
+  "appId": "1:643335247531:web:f85f0d69da05d7d46389ee",
+  "apiKey": "AIzaSyBtHhx8hKNh-6cA_XI4jnAY6ihOtKx-Qz0",
+  "authDomain": "mely-noe-wedding-celebration.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "643335247531"
 };
