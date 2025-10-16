c964bc8e61c8673de114d3c36d2e1b7dd8935a8d
alvearcoria <alexalvear13@gmail.com>
2025-10-16 16:14:29 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  "projectId": "wedding-site-a7398",
-  "appId": "1:245136884394:web:e4e6c10b7b1b3e1b74f3b3",
-  "storageBucket": "wedding-site-a7398.appspot.com",
-  "apiKey": "AIzaSyAZT9I5xJ33a92rYJbWnF6xLzFhQjW1U_w",
-  "authDomain": "wedding-site-a7398.firebaseapp.com",
+  "projectId": "wedding-site-52d3a",
+  "appId": "1:245136884394:web:e23a3b4e433b938374f3b3",
+  "storageBucket": "wedding-site-52d3a.appspot.com",
+  "apiKey": "AIzaSyC2qC-8ZfbPqg_j3pWkF_lT7J_z-vO2vSY",
+  "authDomain": "wedding-site-52d3a.firebaseapp.com",
   "messagingSenderId": "245136884394"
 };
